6fb302b4e70b0af8e80883035ee8c6d08d0ccc14
Abanoup Wagih <bebowagih2000@gmail.com>
2023-01-16 17:07:57 +0200

    Upload The Project

---
+++ js/music-list.js
@@ -0,0 +1,50 @@
+let allMusic = [
+  {
+    name: "tarnemt Getna Bmiladk",
+    artist: "كورال ثمر الروح",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "ترنيمة مش قصة وخلاص",
+    artist: "فريق المخلص",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "  ارض افرحي",
+    artist: "فريق قلب داوود",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "دقي دقي يا اجراس",
+    artist: "الحياة افضل",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "ترنيمة ليك التسبيح",
+    artist: "فريق صدي صوت",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "ترنيمة شكرا يارب مجدا ليك",
+    artist: "فريق المس ايدينا ",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "حبة قش",
+    artist: "كورال قلب داوود",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "ترنيمة صفحة بيضاء",
+    artist: "فريق المس ايدينا",
+    img: "music-8",
+    src: "music-8"
+  }
+];
